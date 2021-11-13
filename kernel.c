7129333e7729d8a5e415d8a40e563c6ad5cbcc99
Matthew Damiano <58225683+matthewdamiano66@users.noreply.github.com>
2021-11-13 15:28:27 -0500

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,44 +1,52 @@
-void handleInterrupt21(int*);
+void handleInterrupt21(int ax, int bx, int cx, int dx);
 void printString(char* s);
 void printChar(char c);
 void readString(char*);
 void readSector(char*,int);
-void makeInterrupt();
+void makeInterrupt21();
 int main(){
-//	char* chars;
-//	char line[80];
-//	char buffer[512];
+	char line [100 ];
 	makeInterrupt21();
 	interrupt(0x21,0,"Enter a line: ",0,0);
-//	interrupt(0x21,1,line,0,0);
-//	interrupt(0x21,2,buffer,30,0);
-//	interrupt(0x21,0,buffer,0,0);
-	while(1);	
-
+//	printString("Enter a line:");
+	readString(line);
+	printString(line);
+	while(1);
 
 }
 void printChar(char c){
-
-
 	interrupt(0x10,0x0e*0x100+c,0,0,0);
-	
 }
 
 void printString(char* s)
 {
 	int i=0;
 	while(s[i]!='\0')
-		printChar(c(i++));
+		printChar(s[i++]);
 }
+
+
+
 	void readString(char* line)
 	{
 	int i=0;
 	while(1){
 	line[i] = interrupt(0x16,0,0,0,0);
 	printChar(line[i]);
-	if(line[1] == '\r')
+	if(line[i] == '\r')
 		{
-	line[i+1]='n';
+	line[i+1]='\n';
+		printChar('\n');
+		line[i+2]='\0';
+		return;	
 		}
+	i++;
 	}
+
+
+}
+void handleInterrupt21(int ax,int bx,int cx,int dx){
+	if (ax ==0)
+		printString(bx);
+
 }
